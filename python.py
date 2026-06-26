864e80e31e93f740745e3167c98331fcbf4f24ec
Beginningsteps <krishna.anuraag02@gmail.com>
2026-06-26 10:32:44 +0000

    lattice

--- python.py
+++ python.py
@@ -1,12 +1,25 @@
 import numpy as np
-import scipy as scp
+import random as rand
 def squared(x):
     return x**2
 
 
 def array(size):
     return np.array([[0]*size]*size)
-print(array(5))
+
+def lattice(size):
+    lattice = array(size)
+    decider = [0,1]
+    for i in (size):
+        for j in (size):
+            if rand.choice([0,1]) == 0:
+                lattice[i][j] = -1
+                else:
+                    lattice[i][j] = 1
+    return lattice
+print(lattice(5))
+
+
 
 
 
