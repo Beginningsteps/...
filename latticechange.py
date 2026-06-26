e7173a075e71577ef2c7f72054bf3dd94927ab70
Beginningsteps <krishna.anuraag02@gmail.com>
2026-06-26 17:59:52 +0000

    graph has less sweeps

--- latticechange.py
+++ latticechange.py
@@ -10,9 +10,8 @@ def sweeps(size, B,J,n):
     f = 0
     deltaE = 0
     r = 0
-    MagnetisationList = []
-    Mag = 0
-    sweepslist = []
+    MagnetisationList = [np.mean(givenlattice)]
+    sweepslist = [0]
     for sweep in range(1,n+1):
         sweepslist = sweepslist + [sweep]
 
@@ -20,7 +19,7 @@ def sweeps(size, B,J,n):
             for j in range(size):
                 r = rand.random()
                 f = givenlattice[i-1][j] + givenlattice[i - size +1][j] + givenlattice[i][j - 1] + givenlattice[i][j - size +1]
-                deltaE =-2*(J*f +B)*givenlattice[i][j]
+                deltaE =2*(J*f +B)*givenlattice[i][j]
                 if np.e**(-deltaE) > 1:
                      givenlattice[i][j] = -givenlattice[i][j]
                 elif np.e**(-deltaE) > r:
@@ -28,7 +27,7 @@ def sweeps(size, B,J,n):
                 else:
                     givenlattice[i][j] = givenlattice[i][j]
         Mag = np.mean(givenlattice)
-        MagnetisationList = MagnetisationList + [Mag]
+        MagnetisationList = MagnetisationList + [np.mean(givenlattice)]
         
         
     return givenlattice, MagnetisationList , sweepslist
--- plotter.py
+++ plotter.py
@@ -3,12 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 from latticechange import sweeps
-basis = sweeps(50, -0.05, 0.95,300)
+basis = sweeps(50, -0.05, 0.95,30)
 xvalues = basis[2]
 yvalues = basis[1]
 
+
 plt.figure()
+plt.xlabel('Number of sweeps')
+plt.ylabel(' Magnetisation of lattice')
 plt.plot(xvalues,yvalues)
+
 
 plt.show()
 plt.savefig('mag graph.png')
